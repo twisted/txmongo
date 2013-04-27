97c5857e267f0c208bc977c15ef145d23d94dbc9
Duncan McGreggor <duncan.mcgreggor@rackspace.com>
2013-04-27 13:44:25 -0700

    Added update example.

---
+++ examples/deferreds/update.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+import sys
+import time
+
+from twisted.internet import reactor
+from twisted.python import log
+
+import txmongo
+
+
+def updateData(ignored, conn):
+    print "updating data..."
+    collection = conn.foo.test
+    d = collection.update(
+        {"foo": "bar"}, {"$set": {"name": "jane doe"}}, safe=True)
+    d.addErrback(log.err)
+    return d
+
+
+def insertData(conn):
+    print "inserting data..."
+    collection = conn.foo.test
+    d = collection.insert({"foo": "bar", "name": "john doe"}, safe=True)
+    d.addErrback(log.err)
+    d.addCallback(updateData, conn)
+    return d
+
+
+def finish(ignore):
+    print "finishing up..."
+    reactor.stop()
+
+
+def example():
+    d = txmongo.MongoConnection()
+    d.addCallback(insertData)
+    d.addCallback(finish)
+    return d
+
+
+if __name__ == '__main__':
+    log.startLogging(sys.stdout)
+    example()
+    reactor.run()
