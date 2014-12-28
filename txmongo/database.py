4ff1d4ee4043addeb31631955fd5714ffd51afc6
Carl D'Halluin <carl@amplidata.com>
2014-12-28 13:54:25 +0100

    fixed import in database.py

--- txmongo/database.py
+++ txmongo/database.py
@@ -3,8 +3,7 @@
 # Use of this source code is governed by the Apache License that can be
 # found in the LICENSE file.
 
-#from bson.son import SON
-#from pymongo import auth
+from bson.son import SON
 from twisted.internet import defer
 from txmongo.collection import Collection
 
