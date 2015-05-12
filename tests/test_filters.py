ffc9ae1f64b9476003cac3a16ea1c5a582f0fad6
Bret Curtis <psi29a@gmail.com>
2015-05-12 22:05:03 +0200

    add coverage for hinting

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -37,9 +37,20 @@ class TestMongoFilters(unittest.TestCase):
         yield self.coll.drop()
         yield self.conn.disconnect()
 
-
     @defer.inlineCallbacks
     def test_Hint(self):
+        # create index and test it is honoured
+        yield self.coll.create_index(qf.sort(qf.ASCENDING("x")), name="test_index")
+        found_1 = yield self.coll.find({}, filter=qf.hint([('x', 1)]))
+        found_2 = yield self.coll.find({}, filter=qf.hint(qf.ASCENDING("x")))
+        found_3 = yield self.coll.find({}, filter=qf.hint("test_index"))
+        self.assertTrue(found_1 == found_2 and found_1 == found_3)
+
+        # find() should fail with 'bad hint' if hint specifier works correctly
+        self.assertFailure(self.coll.find({}, filter=qf.hint(["test_index", 1])), OperationFailure)
+        self.assertFailure(self.coll.find({}, filter=qf.hint(qf.ASCENDING("test_index"))),
+                           OperationFailure)
+
         # Ensure there is no {x:1} index
         yield self.coll.dropIndex([('x', 1)])
 
