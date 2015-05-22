11a2673c832f7dfeae163c7d2cfd4c89f2bc91c4
Ilya Skriblovsky <IlyaSkriblovsky@gmail.com>
2015-05-22 11:19:23 +0300

    Copyrights updated in files created by me

--- tests/test_replicaset.py
+++ tests/test_replicaset.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-# Copyright 2010 Mark L.
+# Copyright 2015 Ilya Skriblovsky <ilyaskriblovsky@gmail.com>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
