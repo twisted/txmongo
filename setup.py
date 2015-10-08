269beedf3c7dd6eee67c48828a5175b9a700a141
Bret Curtis <psi29a@gmail.com>
2015-10-08 22:18:11 +0200

    simplify setup.py

--- setup.py
+++ setup.py
@@ -1,43 +1,16 @@
 #!/usr/bin/env python
-import sys
-import os
-import shutil
-
 from setuptools import setup
-from setuptools import Feature
-from distutils.cmd import Command
-from distutils.command.build_ext import build_ext
-from distutils.errors import CCompilerError
-from distutils.errors import DistutilsPlatformError, DistutilsExecError
-from distutils.core import Extension
-
-
-requirements = ["twisted", "pymongo"]
-try:
-    import xml.etree.ElementTree
-except ImportError:
-    requirements.append("elementtree")
-
-
-if sys.platform == 'win32' and sys.version_info > (2, 6):
-   # 2.6's distutils.msvc9compiler can raise an IOError when failing to
-   # find the compiler
-   build_errors = (CCompilerError, DistutilsExecError, DistutilsPlatformError,
-                 IOError)
-else:
-   build_errors = (CCompilerError, DistutilsExecError, DistutilsPlatformError)
-
 
 setup(
     name="txmongo",
-    version="15.3.0",
+    version="15.3.1",
     description="Asynchronous Python driver for MongoDB <http://www.mongodb.org>",
     author="Alexandre Fiori, Bret Curtis",
     author_email="fiorix@gmail.com, psi29a@gmail.com",
     url="https://github.com/twisted/txmongo",
     keywords=["mongo", "mongodb", "pymongo", "gridfs", "txmongo"],
     packages=["txmongo", "txmongo._gridfs"],
-    install_requires=requirements,
+    install_requires=["twisted>=14.0", "pymongo>=3.0"],
     license="Apache License, Version 2.0",
     include_package_data=True,
     test_suite="nose.collector",
